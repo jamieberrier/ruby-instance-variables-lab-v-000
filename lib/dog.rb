456bf3c8a948a88397affea2f2b7754868d2f2eb
jamieberrier <berrier.jamie@gmail.com>
2019-02-20 18:25:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,31 +1,15 @@
 class Dog
-<<<<<<< HEAD
-
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
-
-=======
  
   def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
  
->>>>>>> 7cd2839850921cc3a5c1bb6f53212c819e973911
   def name
     @this_dogs_name
   end
 end
-<<<<<<< HEAD
 
-lassie = Dog.new
-lassie.name = "Lassie"
-
-puts lassie.name
-=======
- 
 lassie = Dog.new
 lassie.name = "Lassie"
  
 puts lassie.name
->>>>>>> 7cd2839850921cc3a5c1bb6f53212c819e973911
